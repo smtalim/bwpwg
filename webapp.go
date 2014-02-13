0cf61d59f7a2dd278d7b6927e68589d95ba4a604
Satish Talim <satish@rubylearning.org>
2014-02-13 15:58:07 +0530

    commit

---
+++ webapp.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+        http.HandleFunc("/", handler)
+        fmt.Println("listening...")
+        err := http.ListenAndServe(GetPort(), nil)
+        if err != nil {
+                panic(err)
+        }
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello. This is our first GO web program!")
+}
+
+// Get the Port from the environment so we can run on Heroku
+func GetPort() string {
+        var port = os.Getenv("PORT")
+	// Set a default port if there is nothing in the environment
+	if port == "" {
+		port = "4747"
+		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+	}
+	return ":" + port
+}
