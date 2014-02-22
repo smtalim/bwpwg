daaae7d96b9fc353a8fdcb8c87205ed1929a40a2
Satish Talim <satish@rubylearning.org>
2014-02-22 13:08:23 +0530

    code

---
+++ dosasite.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+        "log"
+        "net/http"
+)
+
+func main() {
+        fs := http.FileServer(http.Dir("public"))
+        http.Handle("/", fs)
+
+        log.Println("Listening...")
+        http.ListenAndServe(":3000", nil)
+}
+
