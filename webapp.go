451d256dcc976e4edf49ce42eb9718f1b93bbb22
Satish Talim <satish@rubylearning.org>
2014-04-04 11:16:25 +0530

    commit

--- webapp.go
+++ webapp.go
@@ -16,7 +16,7 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello. This is our first GO web program for Heroku!")
+	fmt.Fprintf(w, "Hello. This is our first Go web program!")
 }
 
 // Get the Port from the environment so we can run on Heroku
