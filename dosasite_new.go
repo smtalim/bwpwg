fc56c2aae6470272ef8268666db7a4af302f9b59
Satish Talim <satish@rubylearning.org>
2014-04-05 09:24:22 +0530

    commit

--- dosasite_new.go
+++ dosasite_new.go
@@ -26,7 +26,7 @@ func GetPort() string {
         var port = os.Getenv("PORT")
 	// Set a default port if there is nothing in the environment
 	if port == "" {
-		port = "3000"
+		port = "4747"
 		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
 	}
 	return ":" + port
